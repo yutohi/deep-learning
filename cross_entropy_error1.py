e82d33d6cd3edc45521b368231dda281539551b8
yutohi <55383824+yutohi@users.noreply.github.com>
2019-09-17 19:20:26 +0900

    Add files via upload

---
+++ cross_entropy_error1.py
@@ -0,0 +1,14 @@
+#交差エントロピー誤差
+#ミニバッチ対応　
+#教師データがone-hot
+
+import numpy as np
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) /  batch_size
+    
---
+++ cross_entropy_error2.py
@@ -0,0 +1,13 @@
+#交差エントロピー誤差
+#ミニバッチ対応　
+#教師データがラベル
+
+import numpy as np
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) /  batch_size
